d518c297b3d585804a6b216c32bd7034aad87679
Jack Ryan Oracoy <jackryanoracoy@gmail.com>
2018-10-28 11:35:58 +0800

    Update: added more font types, remove list of fonts in service worker, minor changes and fixes.

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,76 @@
-const version = "0.1";
-const cacheName = 'jackro-template-${version}';
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-				'/',
-				'index.html',
+var CACHE = 'ServiceWorker-precache';
+var precacheFiles = [
+	'/',
+	'index.html',
 
-				'common/js/jquery-2.1.3.min.js',
-				'common/js/object-fit.min.js',
-				'common/js/custom.min.js',
+	'common/js/jquery-2.1.3.min.js',
+	'common/js/object-fit.min.js',
+	'common/js/custom.min.js',
 
-				'common/css/style.min.css',
-			
-				'common/font-awesome/css/fontawesome-all.min.css',
-			
-				'common/font-awesome/webfonts/fa-brands-400.ttf',
-				'common/font-awesome/webfonts/fa-brands-400.woff',
-				'common/font-awesome/webfonts/fa-brands-400.woff2',
-				'common/font-awesome/webfonts/fa-regular-400.ttf',
-				'common/font-awesome/webfonts/fa-regular-400.woff',
-				'common/font-awesome/webfonts/fa-regular-400.woff2',
-				'common/font-awesome/webfonts/fa-solid-900.ttf',
-				'common/font-awesome/webfonts/fa-solid-900.woff',
-				'common/font-awesome/webfonts/fa-solid-900.woff2',
-			
-				'common/fonts/Roboto/Roboto-Black.ttf',
-				'common/fonts/Roboto/Roboto-Bold.ttf',
-				'common/fonts/Roboto/Roboto-Italic.ttf',
-				'common/fonts/Roboto/Roboto-Light.ttf',
-				'common/fonts/Roboto/Roboto-Medium.ttf',
-				'common/fonts/Roboto/Roboto-Regular.ttf',
-				'common/fonts/Roboto/Roboto-Thin.ttf',
-				'common/fonts/Roboto_Condensed/RobotoCondensed-Bold.ttf',
-				'common/fonts/Roboto_Condensed/RobotoCondensed-Italic.ttf',
-				'common/fonts/Roboto_Condensed/RobotoCondensed-Light.ttf',
-				'common/fonts/Roboto_Condensed/RobotoCondensed-Regular.ttf',
-      ])
-				.then(() => self.skipWaiting());
-    })
-  );
+	'common/css/style.min.css',
+
+	'common/font-awesome/css/fontawesome-all.min.css',
+
+	'common/font-awesome/webfonts/fa-brands-400.ttf',
+	'common/font-awesome/webfonts/fa-brands-400.woff',
+	'common/font-awesome/webfonts/fa-brands-400.woff2',
+	'common/font-awesome/webfonts/fa-regular-400.ttf',
+	'common/font-awesome/webfonts/fa-regular-400.woff',
+	'common/font-awesome/webfonts/fa-regular-400.woff2',
+	'common/font-awesome/webfonts/fa-solid-900.ttf',
+	'common/font-awesome/webfonts/fa-solid-900.woff',
+	'common/font-awesome/webfonts/fa-solid-900.woff2',
+];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('ServiceWorker: The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('ServiceWorker: Skip waiting on install');
+    return self.skipWaiting();
+  }));
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+  console.log('ServiceWorker: Claiming clients for current page');
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(evt) {
+  console.log('ServiceWorker: The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
 });
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+function fromServer(request){
+  //this is the fallback if it is not in the cache to go to the server and get it
+  return fetch(request).then(function(response){ return response});
+}
